6609fc03a88bf57ed3900db0be5915bf86cfc850
abdi42 <abdullahimahamed42@gmail.com>
2016-01-10 20:12:01 +0000

    Added server.js file

---
+++ server/server.js
@@ -0,0 +1,24 @@
+var express = require("express");
+var app  = express();
+var bodyParser = require("body-parser");
+var mongoose = require("mongoose");
+
+app.use(bodyParser.urlencoded({extended:true}));
+app.use(bodyParser.json());
+
+var port = process.env.PORT || 8080;
+
+var router = express.Router();
+
+mongoose.connect('mongodb://localhost/plants',function(err){
+    if(err) {
+        console.log('connection error', err);
+    } else {
+        console.log('connection successful');
+    }
+})
+
+require("./routes/routes.js")(app);
+
+app.listen(port);
+console.log("Running on port " + port);
