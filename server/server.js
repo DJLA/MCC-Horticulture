d20fa98b4744a8fddcd8213ce9a9186a324381ca
abdi42 <abdullahimahamed42@gmail.com>
2016-01-29 18:25:37 -0600

    Allow CORS

--- server/server.js
+++ server/server.js
@@ -5,9 +5,11 @@ var mongoose = require("mongoose");
 
 app.use(function(req, res, next) {
   res.header("Access-Control-Allow-Origin", "*");
+  res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
   next();
 });
+
 
 app.use(bodyParser.urlencoded({extended:true}));
 app.use(bodyParser.json());
